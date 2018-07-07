b7ace6ff554b6f714b9450979c7853d182bc2c5e
Keli Hansen <keli.p.hansen@gmail.com>
2018-07-07 09:40:16 -0700

    adjust feed

--- src/components/feed/FeedShareable.js
+++ src/components/feed/FeedShareable.js
@@ -8,16 +8,15 @@ export default class FeedShareable extends Component {
     name: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     expiration: PropTypes.string,
-    owner: PropTypes.string.isRequired,
-    ownerId: PropTypes.string.isRequired
+    owner: PropTypes.object.isRequired
   };
 
   render() {
-    const { name, expiration, type, owner, ownerId } = this.props;
+    const { name, expiration, type, owner: { _id, firstName } } = this.props;
 
     return (
       <li className={type}>
-        <h3 className="owner"><Link to={`/friends/${ownerId}`}>{owner}</Link> is {type}:</h3>
+        <h3 className="owner"><Link to={`/friends/${_id}`}>{firstName}</Link> is {type}:</h3>
         <div className="content">
           <h3>{name}</h3>
           {expiration && <h3 className="feed-expiration">by {formatDate(expiration)}</h3>}
